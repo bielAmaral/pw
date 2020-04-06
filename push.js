34bb01922ef0307f26adcf06cf99907b590afdb7
Gabriel Amaral <gabritorga@gmail.com>
2020-04-06 11:01:28 -0300

    fix

--- push.js
+++ push.js
@@ -6,6 +6,6 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:gabritorga@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/cYmVQ26k740:APA91bFrpfI_3dJPX9q6ARK5vZPi6ZhLKpdGad65tCSpIyZB15e8IBTU9eiogYgsIMsJpcgnrON-qaHk54eh2nPAJ-RFkaRylktH-dFk5xp11vPdyHlrWSwPecL19XcmfAwPcdfFx-J6","expirationTime":null,"keys":{"p256dh":"BEYfymcPDJd8_adxBeXZFQ8TNNJkAfFo2JTigP2f8Rt9zj7KDwwUFjZtFriduyECFplafsCM62Ctrej_LUubFcw","auth":"V49frsrIdStEK69Lcga_ww"}};
 
 push.sendNotification(sub, 'teste message')
