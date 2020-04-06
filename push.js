3f6b350782eb53fd9876c9beaece6747587b8e75
Gabriel Amaral <gabritorga@gmail.com>
2020-04-06 11:14:04 -0300

    fix

--- push.js
+++ push.js
@@ -7,5 +7,6 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:gabritorga@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/fOCwxawmNJc:APA91bEDZmu67_gIymADXLxc7fT99BtmTYHQspPCoks8_QuCRD07zfVlGhhz2cRx_SNggErNHKMTFGiyfJkPY_fha4l-JrfVk2klgjjFEBLjWO4rXXUsqj0EvYZSlqjXuYbNZJSbOrgM", "expirationTime": null, "keys": { "p256dh": "BD4HAkqzaWnI8ZfPMwP0lymu9OvMK8Z4HaEAT3cuDq9D32OM2jpBekhrJvyfG3nZMAtzH73w_CdqWUsEecUATQQ", "auth": "kldp8sIC8RtjBhO5XMmpfQ" } };
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/fOCwxawmNJc:APA91bEDZmu67_gIymADXLxc7fT99BtmTYHQspPCoks8_QuCRD07zfVlGhhz2cRx_SNggErNHKMTFGiyfJkPY_fha4l-JrfVk2klgjjFEBLjWO4rXXUsqj0EvYZSlqjXuYbNZJSbOrgM","expirationTime":null,"keys":{"p256dh":"BD4HAkqzaWnI8ZfPMwP0lymu9OvMK8Z4HaEAT3cuDq9D32OM2jpBekhrJvyfG3nZMAtzH73w_CdqWUsEecUATQQ","auth":"kldp8sIC8RtjBhO5XMmpfQ"}};
+
 push.sendNotification(sub, 'teste message')
